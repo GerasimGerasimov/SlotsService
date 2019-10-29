a3f96569491b795608792c3dbb654c76abd265f8
Gerasim Gerasimov <dialix@yandex.ru>
2019-10-29 15:02:52 +0700

    установил модуль serialports, подрихтовал crc16 для TS, отправил 11-команду и получил ID в ответ

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import NetPorts from "./netports/netports";
+import ComPort from "./netports/comport"
+
+const settings = {
+    port:'COM3',//название порта
+        settings: { // настройки порта
+              baudRate: 115200, // this is synced to what was set for the Arduino Code
+              dataBits: 8, // this is the default for Arduino serial communication
+              parity: 'none', // this is the default for Arduino serial communication
+              stopBits: 1, // this is the default for Arduino serial communication
+              flowControl: false // this is the default for Arduino serial communication
+          }
+    }
+
+let COMx: NetPorts = new ComPort(settings);
+
+/*
 class Greeting {
  
     private greet: string = 'hello!';
@@ -8,3 +25,4 @@ class Greeting {
 }
  
 let myGreeting = new Greeting();
+*/
---
+++ src/netports/netports.ts
@@ -0,0 +1,11 @@
+export default abstract class NetPorts  {
+    constructor(settings: any) {};
+    abstract configure(settings: any): void;
+    abstract onOpen():void;
+    abstract onClose():void;
+    abstract onError(err: any):void;
+    abstract onDataRead(data: any, err: any):void;
+    abstract get isOpen():boolean;//открыт порт?
+    // abstract read():Promise<string>;
+    // abstract write(msg:string):Promise<boolean>;
+}
